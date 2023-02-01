1ee36d3820331a9ca4e23fe3c41393b8358c378d
Progga <proggaroy2000@gmail.com>
2023-02-02 01:00:10 +0600

    inch to feet and mile to kilo convert

--- inchTofeet.js
+++ inchTofeet.js
@@ -1,5 +1,41 @@
+// inch to feet convert
+
 const myInch = 12;
 const myFeet = myInch/12;
 console.log(myFeet);
 
 
+const dadaInch =144;
+const dadaFeet = dadaInch/12;
+console.log(dadaFeet);
+
+const dadiInch = 90;
+const dadiFeet = dadiInch/12;
+console.log('Dadi feet',dadiFeet);
+
+// using function to convert inch to feet
+
+function convertInchtoFeet(inches){
+
+    const feet = inches/12;
+    return feet;
+
+}
+const giveInches = convertInchtoFeet(140)
+console.log(giveInches);
+
+
+const myRealInch = convertInchtoFeet(150)
+console.log('My inches',myRealInch);
+
+
+// mile to kilometer
+
+function convertMeletokilometer(miles){
+
+const kilometer = miles *1.60934;
+return kilometer;
+
+}
+const giveKilo = convertMeletokilometer(4);
+console.log('Kilometer', giveKilo);
