9e77223b5a65c27147dba385fe1070ddf9ae39bb
Progga <proggaroy2000@gmail.com>
2023-02-02 01:16:15 +0600

    check even

---
+++ iseven.js
@@ -0,0 +1,14 @@
+function checkEvenOrOdd(number){
+    if(number % 2==0){
+       return true
+    }
+   
+    else{
+ return false
+    }
+   
+}
+const givenNumber = checkEvenOrOdd(98);
+console.log(givenNumber)
+const givenNumber2 = checkEvenOrOdd(177);
+console.log(givenNumber2)
