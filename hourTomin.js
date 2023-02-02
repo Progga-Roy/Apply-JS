6cc8841fca8e21c81313f69f7d81bddc290d2aaf
Progga <proggaroy2000@gmail.com>
2023-02-02 23:37:10 +0600

    convert time : hour to min, min to sec

---
+++ hourTomin.js
@@ -0,0 +1,20 @@
+// hour to minutes
+
+function convertHourToMin(hour){
+let checkHour = hour*60;
+
+return checkHour;
+}
+
+const giveHour= convertHourToMin(4)
+console.log(giveHour);
+
+// minutes to hour
+
+function convertMinToSec(hour){
+    let checkMin = hour*60;
+    
+    return checkMin;
+    }
+    const giveMin= convertMinToSec(5)
+    console.log(giveMin);
